bef13667b2f0bf8181624f9f8f8bc73a2442b785
Immah007 <112688332+Immah007@users.noreply.github.com>
2024-10-25 18:37:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const pdfUrl = 'emmanuel_baraka_portfolio.pdf'; // The PDF file is in the same directory as your HTML file
+        
 document.getElementById('home-btn').addEventListener('click', function() {
             const link = document.createElement('a');
             link.href = pdfUrl;
